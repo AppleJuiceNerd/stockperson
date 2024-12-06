c0282d4ac0aa14e4ae3605d820105ea5700ba7f8
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2024-12-05 21:37:24 -0600

    Reformat objects.py with tabs (i like tabs)

--- data-generator/chapter-2.0/objects.py
+++ data-generator/chapter-2.0/objects.py
@@ -2,37 +2,37 @@ import datetime
 
 # Represents a product
 class Product:
-    def __init__(self, name: str, price: float):
+	def __init__(self, name: str, price: float):
 
-        self.name = name
-        self.price = price
+		self.name = name
+		self.price = price
 
 # Represents a line in an invoice
 class InvoiceLine:
-    def __init__(
-        self,
-        lineNo: int,
-        product: Product,
-        quantity: int,
-    ):
+	def __init__(
+		self,
+		lineNo: int,
+		product: Product,
+		quantity: int,
+	):
 
-        self.lineNo = lineNo
-        self.product = product
-        self.quantity = quantity
+		self.lineNo = lineNo
+		self.product = product
+		self.quantity = quantity
 
 
 # Represents a sales invoice
 class Invoice:
-    def __init__(
-        self,
-        docNo: str,
-        date: datetime.date,
-        customer: str,
-        discount: float,
-        lines: list,
-    ):
+	def __init__(
+		self,
+		docNo: str,
+		date: datetime.date,
+		customer: str,
+		discount: float,
+		lines: list,
+	):
 
-        self.docNo = docNo
-        self.date = date
-        self.customer = customer
-        self.lines = lines
+		self.docNo = docNo
+		self.date = date
+		self.customer = customer
+		self.lines = lines
